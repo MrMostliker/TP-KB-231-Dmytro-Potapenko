63e50df18bcbf8e108b7821083fd20b70d49e631
MrMostliker <mrmostliker@gmail.com>
2024-11-19 19:31:56 +0200

    completed tasks for topic 4

--- topic_04/task1-2.py
+++ topic_04/task1-2.py
@@ -48,28 +48,28 @@ def calculator():
             break
         
         # Validate the choice of operation
-        if choice in ['1', '2', '3', '4', '5', '6', '7']:
+        if choice in ['1', '2', '3', '4', '5', '6', '7', '+','-','*','/','%','**','//',]:
             try:
                 num1 = float(input("Enter the first number: "))
                 num2 = float(input("Enter the second number: "))
             except ValueError:
-                print("Invalid input! Please enter numerical values.")
+                print("Invalid input! Please enter numerical or symbolical values.")
                 continue
             
             match choice:
-                case '1':
+                case '1' | '+':
                     print(f"Result: {add(num1, num2)}")
-                case '2':
+                case '2' | '-':
                     print(f"Result: {subtract(num1, num2)}")
-                case '3':
+                case '3' | '*':
                     print(f"Result: {multiply(num1, num2)}")
-                case '4':
+                case '4' | '/':
                     print(f"Result: {divide(num1, num2)}")
-                case '5':
+                case '5' | '%':
                     print(f"Result: {modulus(num1, num2)}")
-                case '6':
+                case '6' | '**':
                     print(f"Result: {exponent(num1, num2)}")
-                case '7':
+                case '7' | '//':
                     print(f"Result: {floor_division(num1, num2)}")
         else:
             print("Invalid choice! Please select a valid operation.")
